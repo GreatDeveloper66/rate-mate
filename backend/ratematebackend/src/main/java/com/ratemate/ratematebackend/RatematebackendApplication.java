70d8e88424b11cd22359ecf642761227da09fe98
GreatDeveloper66 <ashaffe@gmail.com>
2025-05-12 15:30:00 -0500

    Add Java Backend

---
+++ backend/ratematebackend/src/main/java/com/ratemate/ratematebackend/RatematebackendApplication.java
@@ -0,0 +1,13 @@
+package com.ratemate.ratematebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RatematebackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RatematebackendApplication.class, args);
+	}
+
+}
---
+++ backend/ratematebackend/src/test/java/com/ratemate/ratematebackend/RatematebackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ratemate.ratematebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RatematebackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
